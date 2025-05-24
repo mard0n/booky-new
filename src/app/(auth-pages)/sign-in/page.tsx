f718925d37fb48238f434870648f35365ba7d1b4
Mardon <github@mrdn.slmail.me>
2025-05-24 13:54:15 +0500

    Bug fixed

--- src/app/(auth-pages)/sign-in/page.tsx
+++ src/app/(auth-pages)/sign-in/page.tsx
@@ -1,4 +1,4 @@
-import { signInAction, signUpAction } from "~/app/actions";
+import { signInAction } from "~/app/actions";
 import { FormMessage, type Message } from "~/components/form-message";
 // import { SubmitButton } from "@/components/submit-button";
 // import { Input } from "@/components/ui/input";
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { createClient } from "~/utils/supabase/server";
 import { redirect } from "next/navigation";
 import { Input } from "~/components/ui/input";
-import { Butt, Button, CardContenton } from "~/components/ui/button";
+import { Button } from "~/components/ui/button";
 import {
   Card,
   CardHeader,
@@ -29,38 +29,6 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
   }
 
   return (
-    // <form className="flex-1 flex flex-col min-w-64">
-    //   <h1 className="text-2xl font-medium">Tizimga kirish</h1>
-    //   <p className="text-sm text-foreground">
-    //     Akkauntingiz yo&apos;qmi?{" "}
-    //     <Link className="text-foreground font-medium underline" href="/sign-up">
-    //       Ro&apos;yxatdan o&apos;tish
-    //     </Link>
-    //   </p>
-    //   <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-    //     <label htmlFor="email">Email</label>
-    //     <input name="email" placeholder="siz@misol.com" required />
-    //     <div className="flex justify-between items-center">
-    //       <label htmlFor="password">Parol</label>
-    //       <Link
-    //         className="text-xs text-foreground underline"
-    //         href="/forgot-password"
-    //       >
-    //         Parolni unutdingizmi?
-    //       </Link>
-    //     </div>
-    //     <input
-    //       type="password"
-    //       name="password"
-    //       placeholder="Parolingiz"
-    //       required
-    //     />
-    //     <button formAction={signInAction}>
-    //       Kirish
-    //     </button>
-    //     <FormMessage message={searchParams} />
-    //   </div>
-    // </form>
     <div className="relative flex min-h-[calc(100vh-257px)] w-full items-center p-8">
       <div className="z-10 ml-30 w-full max-w-md shrink-0">
         <Card className="w-full rounded-none">
